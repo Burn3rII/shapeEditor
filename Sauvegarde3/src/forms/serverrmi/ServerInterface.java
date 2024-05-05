946858f27b15f8a5769829a2d39565562ebf700b
Simon Antropius <antropius.simon00@gmail.com>
2024-05-05 08:10:19 +0200

    Delete Sauvegarde3/src/forms directory

--- Sauvegarde3/src/forms/serverrmi/ServerInterface.java
+++
@@ -1,9 +0,0 @@
-package forms.serverrmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerInterface extends Remote {
-    void saveDistant(String[] strings) throws RemoteException;
-    String[] loadDistant() throws RemoteException;
-}
